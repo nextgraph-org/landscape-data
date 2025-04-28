d39302bdc5d33cd679e3afc6b5e9da453fcceca4
Niko PLP <niko@nextgraph.org>
2025-04-29 00:00:29 +0300

    first version of landscape data for nextgraph

--- data.js
+++ data.js
@@ -3,39 +3,75 @@ import { LandscapeSchema } from '@localfirstfm/landscape-schema'
 
 export const data = LandscapeSchema.make({
 	Version: 1,
-	Id: 'supersync',
-	Name: 'SuperSync',
-	Description: 'A cool way to sync data',
-	Website: 'https://supersync.cool',
-	Deployment: ['Self-hosted'],
+	Id: 'nextgraph',
+	Name: 'NextGraph',
+	Description: 'Decentralized and local-first web 3.0 ecosystem (apps and framework)',
+	Website: 'https://nextgraph.org',
+	Deployment: ['Self-hosted','Hosted'],
 	License: 'MIT',
+	MaturityLevel: 'Alpha',
+	GitHub: 'https://git.nextgraph.org/NextGraph/nextgraph-rs',
+	UniquenessNote: 'convergence of Linked Data, P2P, and local-first',
+	InitialReleaseDate: '2024-09-02',
+	Score: {
+		MultiDeviceCrossDevice: { data: 'Yes'},
+		MultiDeviceDecentralized: { data: 'Yes', comment: 'replication with a federation of servers. no single point of failure' },
+		OfflineSupportWrites: { data: 'Yes' },
+		OfflineSupportArbitraryQueries: { data: 'Yes', comment: 'offline support for SPARQL on all local documents'},
+		UserOwnedData: { data: 'Yes'},
+		CollaborationMultiEdit: { data: 'Yes'},
+		CollaborationConflictingWrites: { data: 'Yes'},
+		SecurityE2EE: { data: 'Yes'},
+		SecurityUserKeys: { data: 'Yes'},
+		LongevityNoProprietaryServer: { data: 'Yes'}
+	},
 	AppTarget: {
-		LanguageSDK: { data: ['typescript'] },
+		Platform: { data: ['Browser', 'Node', 'iOS', 'Android', 'macOS', 'WASM', 'Linux'] },
+		LanguageSDK: { data: ['TypeScript', 'JavaScript', 'Rust', 'Python'] },
+		FrameworkIntegrations: { data: ['React', 'Svelte'] }
 	},
 	Networking: {
-		Topology: { data: 'Client-Server' },
+		Protocol: { data: 'WebSockets' },
+		Topology: { data: 'P2P via Relay Servers' },
 	},
 	ServerSideData: {
-		PersistenceMechanism: { data: ['N/A'] },
-		DataModelParadigm: { data: 'Relational' },
+		PersistenceMechanism: { data: 'Custom' },
+		DataModelParadigm: { data: 'Document' }
 	},
 	ClientSideData: {
-		QueryAPI: { data: ['Async'] },
-		PersistenceMechanism: { data: ['IndexedDB', 'OPFS'] },
-		PersistenceFeatures: { data: 'Indexes' },
-		DataModel: { data: 'Document' },
+		QueryAPI: { data: ['Async', 'Sync', 'Signals-based Reactivity'] },
+		PersistenceMechanism: { data: ['Yjs', 'OPFS', 'RocksDB'] },
+		PersistenceFeatures: { data: 'Indexes', comment: 'also with Full-text search and Transactions' },
+		DataModel: { data: 'Document', comment: 'Document-oriented and relational (RDF)' },
+		SchemaManagement: { data: ['Schema definition', 'Schema validation on write'] },
 		OfflineReads: { data: 'Full Support' },
-		OfflineWrites: { data: 'Local conflict resolution' },
-		DataSize: { data: 'Up to 5-10 MB per document' },
+		OptimisticUpdates: { data: 'Yes'},
+		OfflineWrites: { data: 'Local conflict resolution' }
 	},
 	SynchronizationStrategy: {
-		FullOrPartialReplication: { data: ['Full Replication'] },
-		ConflictHandling: { data: 'Automatic via CRDT' },
+		FullOrPartialReplication: { data: ['Full Replication', 'Partial Replication'] },
+		ConflictHandling: { data: 'Automatic via CRDT', comment: 'supports Automerge, Yjs and RDF' },
 		WhereResolutionOccurs: { data: 'Client' },
 		WhatGetsSynced: {
 			data: {
-				ClientToClient: 'Ops',
+				ClientToClient: 'Commits',
 			},
 		},
+		Authority: { data: 'Decentralized' }
 	},
+	AuthIdentity: {
+		Encryption: { data: 'Yes', comment: 'end-to-end encryption + encryption at rest' },
+		AuthenticationMethod: { data: ['Built-in', 'Public keys'] },
+		AuthorizationPermissions: { data: ['ACLs'] }
+	},
+	UIRelated: {
+		RichTextEditing: { data: 'Yes' },
+		Components: { data: ['reusable Editors', 'reusable Viewers', 'Auth with Wallet', 'Malleable software'] }
+	},
+	DevelopmentWorkflowsDX: {
+		CLI: { data: 'Command line interface tools for developers and end-users'},
+		TypeSupport: {
+			data: 'type support via LDO',
+		},
+	}
 })
--- details.md
+++ details.md
@@ -1,15 +1,15 @@
-# SuperSync
+# NextGraph
 
-SuperSync is a cool way to sync your data.
+Build local-first, collaborative and decentralized apps, with CRDTs, and end-to-end encryption.
 
 ## Features
 
-- **SuperSync**: Sync your data with ease.
-- **Local-first**: Your data is stored locally, ensuring privacy and security.
+- **Linked Data**: link documents and data together with RDF and DID URIs, then query all your data with SPARQL. Comaptible with Solid.
+- **Social Network**: NextGraph offers decentralized social network features (DM, timeline, share). Compatible with ActivityPub
+- **Portable & Interoperable**: Your data always remains under your control. It isn't tied to a specific app, format, domain name or server.
+- **Malleable Software**: switch viewers and editors and reuse them at will. Enjoy ready-made components that can be reused.
+- **Local-first**: Your data is stored locally, ensuring availability and privacy.
+- **End-to-end encrypted**: All documents are encrypted in the client, and the servers (brokers) cannot see their content.
 - **Cross-platform**: Works on all major platforms.
-- **Open-source**: The code is available for anyone to use and contribute to.
-- **Community-driven**: Join our community and help us improve the project.
+- **Open-source**: The code is available for anyone to use and contribute to, in permissive licensing.
 
-## More info
-
-...
